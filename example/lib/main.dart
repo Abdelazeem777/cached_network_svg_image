7784a41d4e5402d7130080de8349ae14609ab14b
Abdelazeem Kuratem <abdelazeem263@gmail.com>
2022-12-20 12:16:56 +0200

    first commit

---
+++ example/lib/main.dart
@@ -0,0 +1,78 @@
+import 'package:cached_network_svg_image/cached_network_svg_image.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Cached Network SVG Image Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  static const _imagesUrlList = [
+    'https://upload.wikimedia.org/wikipedia/commons/0/02/SVG_logo.svg',
+    'https://upload.wikimedia.org/wikipedia/commons/7/71/Epsilon_letter%2C_Mega_Etymologikon%2C_1499.svg',
+    'https://upload.wikimedia.org/wikipedia/commons/0/0a/Python.svg',
+    'https://upload.wikimedia.org/wikipedia/commons/3/3f/Git_icon.svg',
+    'https://upload.wikimedia.org/wikipedia/commons/9/96/Sass_Logo_Color.svg',
+    'https://upload.wikimedia.org/wikipedia/commons/2/27/PHP-logo.svg',
+    'https://upload.wikimedia.org/wikipedia/commons/5/57/Delta_letter%2C_Mega_Etymologikon%2C_1499.svg',
+    'https://upload.wikimedia.org/wikipedia/commons/9/91/Electron_Software_Framework_Logo.svg',
+    'https://upload.wikimedia.org/wikipedia/commons/e/e2/202009_Allosaurus_fragilis.svg',
+    'https://upload.wikimedia.org/wikipedia/commons/6/61/HTML5_logo_and_wordmark.svg',
+    'https://upload.wikimedia.org/wikipedia/commons/d/d5/CSS3_logo_and_wordmark.svg',
+    'https://upload.wikimedia.org/wikipedia/commons/7/73/Ruby_logo.svg',
+    'https://upload.wikimedia.org/wikipedia/commons/3/34/202101_Zebrafish.svg',
+    'https://upload.wikimedia.org/wikipedia/commons/1/18/ISO_C%2B%2B_Logo.svg',
+    'https://upload.wikimedia.org/wikipedia/commons/b/bb/Lamda_letter%2C_Mega_Etymologikon%2C_1499.svg',
+  ];
+
+  int _selectedIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Cached Network SVG Image Demo'),
+      ),
+      body: Center(
+        child: CachedNetworkSVGImage(
+          _imagesUrlList[_selectedIndex],
+          key: ValueKey(_selectedIndex),
+          placeholder: const CircularProgressIndicator(color: Colors.green),
+          errorWidget: const Icon(Icons.error, color: Colors.red),
+          width: 250.0,
+          height: 250.0,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _nextImage,
+        tooltip: 'Next',
+        child: const Icon(Icons.navigate_next),
+      ),
+    );
+  }
+
+  void _nextImage() => setState(() {
+        _selectedIndex = (_selectedIndex + 1) % _imagesUrlList.length;
+      });
+}
---
+++ lib/cached_network_svg_image.dart
@@ -0,0 +1,152 @@
+library cached_network_svg_image;
+
+import 'dart:developer';
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_cache_manager/flutter_cache_manager.dart';
+
+class CachedNetworkSVGImage extends StatefulWidget {
+  const CachedNetworkSVGImage(
+    String url, {
+    super.key,
+    Widget? placeholder,
+    Widget? errorWidget,
+    double? width,
+    double? height,
+    BoxFit fit = BoxFit.contain,
+    AlignmentGeometry alignment = Alignment.center,
+    bool matchTextDirection = false,
+    bool allowDrawingOutsideViewBox = false,
+    Color? color,
+    BlendMode colorBlendMode = BlendMode.srcIn,
+    String? semanticsLabel,
+    bool excludeFromSemantics = false,
+    Clip clipBehavior = Clip.hardEdge,
+    bool cacheColorFilter = false,
+    SvgTheme? theme,
+  })  : _url = url,
+        _placeholder = placeholder,
+        _errorWidget = errorWidget,
+        _width = width,
+        _height = height,
+        _fit = fit,
+        _alignment = alignment,
+        _matchTextDirection = matchTextDirection,
+        _allowDrawingOutsideViewBox = allowDrawingOutsideViewBox,
+        _color = color,
+        _colorBlendMode = colorBlendMode,
+        _semanticsLabel = semanticsLabel,
+        _excludeFromSemantics = excludeFromSemantics,
+        _clipBehavior = clipBehavior,
+        _cacheColorFilter = cacheColorFilter,
+        _theme = theme;
+
+  final String _url;
+  final Widget? _placeholder;
+  final Widget? _errorWidget;
+  final double? _width;
+  final double? _height;
+  final BoxFit _fit;
+  final AlignmentGeometry _alignment;
+  final bool _matchTextDirection;
+  final bool _allowDrawingOutsideViewBox;
+  final Color? _color;
+  final BlendMode _colorBlendMode;
+  final String? _semanticsLabel;
+  final bool _excludeFromSemantics;
+  final Clip _clipBehavior;
+  final bool _cacheColorFilter;
+  final SvgTheme? _theme;
+
+  @override
+  State<CachedNetworkSVGImage> createState() => _CachedNetworkSVGImageState();
+}
+
+class _CachedNetworkSVGImageState extends State<CachedNetworkSVGImage>
+    with SingleTickerProviderStateMixin {
+  bool _isLoading = true;
+  bool _isError = false;
+  File? _imageFile;
+
+  late final DefaultCacheManager _cacheManager;
+  late final AnimationController _controller;
+  late final Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _cacheManager = DefaultCacheManager();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300),
+    );
+    _animation = Tween(begin: 0.0, end: 1.0).animate(_controller);
+    _loadImage();
+  }
+
+  Future<void> _loadImage() async {
+    try {
+      final file = await _cacheManager.getSingleFile(widget._url);
+      setState(() {
+        _imageFile = file;
+        _isLoading = false;
+      });
+      _controller.forward();
+    } catch (e) {
+      log('CachedNetworkSVGImage: $e');
+      setState(() {
+        _isError = true;
+        _isLoading = false;
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (_isLoading) return _buildPlaceholderWidget();
+
+    if (_isError) return _buildErrorWidget();
+
+    return FadeTransition(
+      opacity: _animation,
+      child: _buildSVGImage(),
+    );
+  }
+
+  Widget _buildPlaceholderWidget() => widget._placeholder ?? const SizedBox();
+
+  Widget _buildErrorWidget() => widget._errorWidget ?? const SizedBox();
+
+  Widget _buildSVGImage() {
+    try {
+      final child = SvgPicture.file(
+        _imageFile!,
+        fit: BoxFit.cover,
+        width: widget._width,
+        height: widget._height,
+        alignment: widget._alignment,
+        matchTextDirection: widget._matchTextDirection,
+        allowDrawingOutsideViewBox: widget._allowDrawingOutsideViewBox,
+        color: widget._color,
+        colorBlendMode: widget._colorBlendMode,
+        semanticsLabel: widget._semanticsLabel,
+        excludeFromSemantics: widget._excludeFromSemantics,
+        clipBehavior: widget._clipBehavior,
+        cacheColorFilter: widget._cacheColorFilter,
+        theme: widget._theme,
+      );
+      return child;
+    } catch (e) {
+      log('CachedNetworkSVGImage: $e');
+      return _buildErrorWidget();
+    }
+  }
+}
