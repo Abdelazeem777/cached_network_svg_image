9d2abd937beb6399b047885ac5ddfa9edff8b81a
Abdelazeem Kuratem <abdelazeem263@gmail.com>
2024-12-16 23:48:08 +0200

    Update dependencies and configuration for Flutter compatibility

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,8 @@
 import FlutterMacOS
 import Foundation
 
-import path_provider_macos
-import sqflite
+import path_provider_foundation
+import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
